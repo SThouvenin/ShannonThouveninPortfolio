7dd92ddd593c6c012d6c73338614e6fd5d647b61
Shannon Thouvenin <sthoovy@gmail.com>
2022-11-19 13:55:22 -0500

    Added directory to React App

---
+++ shannonthouvenin/src/component/Portfolio/Porfolio.tsx
@@ -0,0 +1,10 @@
+import { defaultMaxListeners } from 'events';
+import React, { FunctionComponent } from 'react';
+
+const Portfolio: FunctionComponent = (): JSX.Element => {
+    return (
+        <div>Hello!</div>
+    )
+}
+
+export default Portfolio;
